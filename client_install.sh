7b2249b9c63e9b6c81cb8153595ba05f61fbe815
Extense Consulting <43435978+extense-consulting@users.noreply.github.com>
2021-12-30 15:07:05 +0100

    Delete client_install.sh

--- client_install.sh
+++
@@ -1,7 +0,0 @@
-sudo apt-get update
-sudo apt-get upgrade -y
-curl -sSL https://get.docker.com | sh
-sudo systemctl enable docker
-curl -s https://install.zerotier.com | sudo bash
-sudo zerotier-cli join af78bf94366d3f6c
-sudo reboot
