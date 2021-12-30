2db2ccc63b31870e7609008568180c99aea77f5b
Extense Consulting <43435978+extense-consulting@users.noreply.github.com>
2021-12-30 13:06:38 +0100

    Add files via upload

---
+++ client_install.sh
@@ -0,0 +1,15 @@
+
+
+#CLIENT
+
+sudo apt-get update
+sudo apt-get upgrade -y
+
+curl -sSL https://get.docker.com | sh
+sudo systemctl enable docker
+
+curl -s https://install.zerotier.com | sudo bash
+sudo zerotier-cli join af78bf94366d3f6c
+
+sudo reboot
+
