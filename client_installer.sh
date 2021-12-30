c0bc28992d4ab656d747a37487edf41ecec2ceb4
Extense Consulting <43435978+extense-consulting@users.noreply.github.com>
2021-12-30 17:00:02 +0100

    Update client_installer.sh

--- client_installer.sh
+++ client_installer.sh
@@ -8,4 +8,6 @@ sudo systemctl enable docker
 curl -s https://install.zerotier.com | sudo bash
 sudo zerotier-cli join $1
 sudo zerotier-cli listnetworks
+sudo wget https://raw.githubusercontent.com/extense-consulting/raspiconfig/main/client_led.service /lib/systemd/system/led.service
+sudo systemctl enable led.service
 sudo reboot
