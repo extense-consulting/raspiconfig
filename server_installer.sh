dbecd652b9fe7c53e534099ee28fde6faaec0b8f
Extense Consulting <43435978+extense-consulting@users.noreply.github.com>
2021-12-31 16:02:47 +0100

    Update server_installer.sh

--- server_installer.sh
+++ server_installer.sh
@@ -2,9 +2,19 @@ echo "Installing Server..."
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo rpi-update -y
+sudo apt-get install jq -y
 curl -sSL https://get.docker.com | sh
 sudo systemctl enable docker
+
+echo "Installing ZeroTier..."
 curl -s https://install.zerotier.com | sudo bash
+sudo zerotier-idtool initmoon identity.public >> /home/pi/moon.json
+sudo zerotier-idtool genmoon /home/pi/moon.json
+sudo mkdir /var/lib/zerotier-one/moons.d
+sudo mv *.moon moons.d/
+echo "Your Moon ID: " jq '.id' /home/pi/moon.json
+
+echo "Installing LED Service..."
 sudo wget https://raw.githubusercontent.com/extense-consulting/raspiconfig/main/server_led.service -O /lib/systemd/system/led.service
 sudo systemctl daemon-reload
 sudo systemctl enable led.service
