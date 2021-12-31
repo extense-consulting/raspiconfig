d2d005009e85addce9d6f10b5923d726c44efb12
Extense Consulting <43435978+extense-consulting@users.noreply.github.com>
2021-12-31 14:27:59 +0100

    Update server_installer.sh

--- server_installer.sh
+++ server_installer.sh
@@ -5,7 +5,7 @@ sudo rpi-update -y
 curl -sSL https://get.docker.com | sh
 sudo systemctl enable docker
 curl -s https://install.zerotier.com | sudo bash
-sudo wget https://raw.githubusercontent.com/extense-consulting/raspiconfig/main/client_led.service -O /lib/systemd/system/led.service
+sudo wget https://raw.githubusercontent.com/extense-consulting/raspiconfig/main/server_led.service -O /lib/systemd/system/led.service
 sudo systemctl daemon-reload
 sudo systemctl enable led.service
 sudo reboot
