04858f767447b044037edb1f9c97c12492dca162
Extense Consulting <43435978+extense-consulting@users.noreply.github.com>
2022-01-12 17:38:20 +0100

    Update tor_server_installation.sh

--- tor_server_installation.sh
+++ tor_server_installation.sh
@@ -8,14 +8,14 @@ sudo systemctl stop tor@default.service
 
 sudo cp /etc/tor/torrc /etc/tor/torrc.bak
 
-sudo echo "RunAsDaemon 1" >> /etc/tor/torrc
-sudo echo "ORPort 5000 IPv4Only" >> /etc/tor/torrc
-sudo echo "ExtORPort auto" >> /etc/tor/torrc
-sudo echo "BridgeRelay 1" >> /etc/tor/torrc
-sudo echo "PublishServerDescriptor 0" >> /etc/tor/torrc
-sudo echo "ServerTransportPlugin obfs4 exec /usr/bin/obfs4proxy" >> /etc/tor/torrc
-sudo echo "ServerTransportListenAddr obfs4 0.0.0.0:4433" >> /etc/tor/torrc
-sudo echo "DNSPort 127.0.0.1:53" >> /etc/tor/torrc
-sudo echo "Nickname $1" >> /etc/tor/torrc
+echo "RunAsDaemon 1" | sudo tee -a /etc/tor/torrc > /dev/null
+echo "ORPort 5000 IPv4Only" | sudo tee -a /etc/tor/torrc > /dev/null
+echo "ExtORPort auto" | sudo tee -a /etc/tor/torrc > /dev/null
+echo "BridgeRelay 1" | sudo tee -a /etc/tor/torrc > /dev/null
+echo "PublishServerDescriptor 0" | sudo tee -a /etc/tor/torrc > /dev/null
+echo "ServerTransportPlugin obfs4 exec /usr/bin/obfs4proxy" | sudo tee -a /etc/tor/torrc > /dev/null
+echo "ServerTransportListenAddr obfs4 0.0.0.0:4433" | sudo tee -a /etc/tor/torrc > /dev/null
+echo "DNSPort 127.0.0.1:53" | sudo tee -a /etc/tor/torrc > /dev/null
+echo "Nickname $1" | sudo tee -a /etc/tor/torrc > /dev/null
 
 sudo systemctl start tor@default.service
